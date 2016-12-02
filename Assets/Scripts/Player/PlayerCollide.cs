fa0fab36b4b60d1a31cc642398dce3304b9d2387
LWWenz <leongww7@gmail.com>
2016-12-03 03:28:52 +0800

    Added Invulnable status in PlayerCollide.cs

--- Assets/Scripts/Player/PlayerCollide.cs
+++ Assets/Scripts/Player/PlayerCollide.cs
@@ -6,17 +6,53 @@ public class PlayerCollide : MonoBehaviour {
 
 	//private Rigidbody2D rb2d;
 	private PlayerStats ps;
+    public bool notInvul, alphaIncDec;
+    public double invulTimer;
+    private SpriteRenderer sr;
+    private Color color;
 
 	void Start () {
 		//rb2d = this.gameObject.GetComponent<Rigidbody2D> ();
 		ps = this.gameObject.GetComponent<PlayerStats> ();
+        sr = this.gameObject.GetComponent<SpriteRenderer>();
+        color = sr.color;
+        notInvul = true;
+        alphaIncDec = true;
+        invulTimer = 0;
 	}
 
+    void Update ()
+    {
+        if(!notInvul)
+        {
+            invulTimer -= Time.deltaTime;
+            if(sr.enabled)
+            {
+                sr.enabled = !sr.enabled;
+            }
+            else
+            {
+                sr.enabled = !sr.enabled;
+            }
+
+            if (invulTimer <= 0)
+            {
+                notInvul = !notInvul;
+                sr.enabled = true;
+            }
+            
+        }
+    }
 	void OnTriggerEnter2D (Collider2D col) {
 		if (col.gameObject.tag == "enemy_bullet") {
 			//ps.decreaseEnergy (4);
-			ps.decreaseHealth (1);
-			Destroy (col.gameObject);
+            if(notInvul)
+            {
+                notInvul = !notInvul;
+                ps.decreaseHealth(1);
+                invulTimer = 2;
+                Destroy(col.gameObject);
+            }
 		}
 	}
 }
