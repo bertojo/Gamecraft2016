185f06eb3f71c69845a0471dd9c302a08baf0290
Albert Yeoh <bertthings@gmail.com>
2016-12-03 04:31:30 +0800

    skill icons with the meter done

--- Assets/IconScript.cs
+++ Assets/IconScript.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EnergyMeterScript : MonoBehaviour {
-
-	public GameObject meterFill;
+public class IconScript : MonoBehaviour {
 
 	public GameObject player;
 	private PlayerStats ps;
+	public Sprite dulled;
+	public Sprite normal;
+	public float activationLevel;
 	private int energy;
-	private float perc;
 
 	// Use this for initialization
 	void Start () {
@@ -20,9 +20,10 @@ public class EnergyMeterScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		energy = ps.getEnergy ();
-		perc = energy / 100.0f;
-		Debug.Log ("PERC: " + perc);
-		Vector3 scale = new Vector3(1, perc, 1);
-		meterFill.transform.localScale = scale;
+		if (energy >= activationLevel) {
+			this.GetComponent<Image> ().sprite = normal;
+		} else {
+			this.GetComponent<Image> ().sprite = dulled;
+		}
 	}
 }
