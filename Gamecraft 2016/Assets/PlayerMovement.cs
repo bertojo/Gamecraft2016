f74a786723dd5ec85ce44361e178433d47184ef3
LWWenz <leongww7@gmail.com>
2016-12-02 19:01:40 +0800

    Test

---
+++ Gamecraft 2016/Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    int speed = 0;
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 5;
+        }
+        else
+        {
+            speed = 10;
+        }
+        if(Input.GetKey("w"))
+        {
+            transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime);
+        }
+        if(Input.GetKey("s"))
+        {
+            transform.Translate(new Vector3(0, -speed, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(new Vector3(-speed, 0, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+        }
+
+    }
+}
