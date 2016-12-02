591857124eed52ce3a2adf4c98c46fe5532efe16
Albert Yeoh <bertthings@gmail.com>
2016-12-02 21:46:40 +0800

    compile everything

---
+++ Assets/Scripts/Player/InstantiateBullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateBullet : MonoBehaviour {
+    public double timer;
+    public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0.1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer <= 0)
+        {
+ 	       Instantiate(bulletPrefab, (this.transform.position), Quaternion.identity);            
+            timer = 0.1;
+        }
+        timer -= Time.deltaTime;
+	}
+}
