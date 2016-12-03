3cf69227ea93b015c3576b5365718ab145312056
Albert Yeoh <bertthings@gmail.com>
2016-12-03 12:46:49 +0800

    pause menu upgrade

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
 	private bool isPaused = false;
-
+	public GameObject resume;
+	public GameObject exit;
 
 	// Use this for initialization
 	void Start () {
-		
+		resume.SetActive (false);
+		exit.SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -21,9 +25,20 @@ public class GameManager : MonoBehaviour {
 		isPaused = !isPaused;
 		if (isPaused) {
 			Time.timeScale = 0;
+			resume.SetActive (true);
+			exit.SetActive (true);
+
 		} else { 
 			Time.timeScale = 1;
+			resume.SetActive (false);
+			exit.SetActive (false);
 		}
+	}
 
+	public void exitToMenu() {
+		Time.timeScale = 1;
+		resume.SetActive (false);
+		exit.SetActive (false);
+		SceneManager.LoadScene (0);
 	}
 }
